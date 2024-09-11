c2635ea561b1e1f2f9a5e89af5d7f4b4bb209919
Sparkworm <imnota.squirp@gmail.com>
2024-09-11 10:17:32 -0500

    Moved code from header file to source file so that changes compile.  Also changed how lines are printed so that they're easier to copy into desmos.

--- include/line.h
+++ include/line.h
@@ -10,19 +10,13 @@ struct line {
   point a;
   point b;
 
-line() : a(point()), b(point()) {}
+  line();// : a(point()), b(point()) {}
   
-line(point point1, point point2) : a(point1), b(point2) {}
+  line(point point1, point point2);// : a(point1), b(point2) {}
+
+  std::string to_string() const;
   
-  std::string to_string() const {
-    return "[" + a.to_string() + "; " + b.to_string() + "]";
-  }
-  
-  friend std::ostream& operator << (std::ostream& os, const line& l) {
-    os << "[" << l.a << "; " << l.b << "]";
-    return os;
-  }
-    
+  friend std::ostream& operator << (std::ostream& os, const line& l);
 };
 
 #endif
--- makefile
+++ makefile
@@ -8,7 +8,7 @@ CXXFLAGS = -std=c++17 -Iinclude
 TARGET = build/PolyProject
 
 # Define the source files
-SRCS = src/main.cpp src/ui.cpp src/utils.cpp src/NGram.cpp
+SRCS = src/main.cpp src/ui.cpp src/utils.cpp src/NGram.cpp src/line.cpp
 
 # Define the object files
 OBJS = $(SRCS:.cpp=.o)
