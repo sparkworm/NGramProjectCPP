c40cfb13372c3e0025ce6f82daa579c983d94fef
Sparkworm <imnota.squirp@gmail.com>
2024-09-19 11:14:29 -0500

    Removed Node.h and replaced it with Connection.h, a struct that represents a connection between two 'nodes' which are actually just uids

---
+++ include/Connection.h
@@ -0,0 +1,19 @@
+/*
+ * This struct represents a connection between two "points" which are just uids
+ */
+
+#ifndef CONNECTION
+#define CONNECTION
+
+struct Connection {
+  Connection();
+  Connection(a,b);
+  
+  unsigned int a;
+  unsigned int b;
+
+  bool has_node(unsigned int node);
+  bool shares_node(Connection other);
+};
+
+#endif
